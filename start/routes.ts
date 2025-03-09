ce9ebaa516c30c4e61bf8059aa96c8d185164d79
Kocannn <dwicandraandika4@gmail.com>
2025-03-09 13:01:25 +0800

    update: group api route with /api/v1

--- start/routes.ts
+++ start/routes.ts
@@ -12,8 +12,11 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 
 router.on('/').render('pages/home')
-
-router.post('/register', [AuthController, 'register']).as('auth.register')
-router.post('/login', [AuthController, 'login']).as('auth.login')
-router.delete('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
-router.get('/me', [AuthController, 'me']).as('auth.me')
+router
+  .group(() => {
+    router.post('/register', [AuthController, 'register']).as('auth.register')
+    router.post('/login', [AuthController, 'login']).as('auth.login')
+    router.delete('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
+    router.get('/me', [AuthController, 'me']).as('auth.me')
+  })
+  .prefix('/api/v1')
