8837790e829334651ff2d6b7d8ab70b75dd52a93
Kocannn <dwicandraandika4@gmail.com>
2025-03-29 17:32:55 +0800

    feat(profile): add education and experience handling
    - Integrated `education` and `experience` fields in the profile page.
    - Updated `handleAddEducation` logic to handle education submissions.
    - Removed unused `formData`, `setFormData`, and `user` variables.
    - Deleted redundant `edit/page.tsx` file to streamline profile editing.
    - Enhanced API to support education and experience data updates.
    
    BREAKING CHANGE: Removed `edit/page.tsx`, migrate to new profile page.

--- app/api/v1/profile/update/route.ts
+++ app/api/v1/profile/update/route.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
+interface Education {
+  institution: string;
+  degree: string;
+  year: string;
+}
 
 export async function PUT(request: Request) {
   const session = await getServerSession();
@@ -9,15 +14,40 @@ export async function PUT(request: Request) {
   }
 
   const data = await request.json();
+  console.log(data);
 
   try {
     const user = await prisma.user.update({
       where: { email: session.user.email },
+      include: {
+        education: true,
+      },
       data: {
         location: data.location,
         bio: data.bio,
       },
     });
+
+    // Handle education data
+    if (data.education && data.education.length > 0) {
+      // Separate existing and new education entries
+      const existingEducation = data.education.filter((item: any) => item.id);
+      const newEducation = data.education
+        .filter((item: any) => !item.id)
+        .map((item: Education) => ({
+          userId: data.id,
+          institution: item.institution,
+          degree: item.degree,
+          year: item.year,
+        }));
+
+      // Create new education entries if any
+      if (newEducation.length > 0) {
+        await prisma.education.createMany({
+          data: newEducation,
+        });
+      }
+    }
 
     return NextResponse.json(user);
   } catch (error) {
